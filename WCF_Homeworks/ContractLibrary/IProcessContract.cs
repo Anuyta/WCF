d8c77990694c23fa8149cba486cb93744debeb80
Anuyta <ryabtseva.anna.92@mail.ru>
2018-03-26 14:53:05 +0300

    howework_1 done
    you can get process ifo

---
+++ WCF_Homeworks/ContractLibrary/IProcessContract.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+
+namespace ContractLibrary
+{
+    [ServiceContract]
+    public interface IProcessContract
+    {
+        [OperationContract]
+        string ShowModulesInfo(string nameProc);
+
+        [OperationContract]
+        string ShowThreadsInfo(string nameProc);
+
+        [OperationContract]
+        string CloseProcess(string nameProc);
+
+        [OperationContract]
+        string StartProcess(string nameProc);
+
+        [OperationContract]
+        string GetProcessByPID(int pid);
+
+        [OperationContract]
+        string GetAllProcess();
+    }
+}
